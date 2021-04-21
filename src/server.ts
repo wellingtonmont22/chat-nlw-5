12767c8bf495afe4907a0ce60ce047dfb79ee5fa
wellingtonmont22 <wellington.m.alves@hotmail.com>
2021-04-21 00:25:28 -0300

    configuração e criação das primeiras rotas

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.get('/', (req, res) => {
+    return res.json({message:"Olá NLW 05!"});
+});
+
+app.post('/', (req, res) => {
+    return res.json({message: "Usuário salvo!"})
+});
+
+app.listen(3000, () => console.log("Rodando... na porta 3000"));
