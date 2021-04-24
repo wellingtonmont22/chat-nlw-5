6058affc8ae9d311cce8f489fc04959c7b5f4ac4
wellingtonmont22 <wellington.m.alves@hotmail.com>
2021-04-24 10:42:52 -0300

    Configurando Ws

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
 import express from "express";
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
+
 import "./database"
 import { routes } from "./routes"
 
 const app = express();
+
+const http = createServer(app);//Criando Protocolo http
+const io = new Server(http);//Criando Protocolo ws
 
 app.use(express.json());
 
