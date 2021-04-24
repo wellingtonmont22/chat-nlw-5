7847dfd590ef07b6326b4320e1658e2bb1800eb7
wellingtonmont22 <wellington.m.alves@hotmail.com>
2021-04-24 11:33:38 -0300

    server com imports de servidor http

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,4 @@
-import express from "express";
-import { createServer } from "http";
-import { Server, Socket } from "socket.io";
-
-import "./database"
-import { routes } from "./routes"
-
-const app = express();
-
-const http = createServer(app);//Criando Servidor - Protocolo http
-const io = new Server(http);//Criando Servidor - Protocolo ws
-
-io.on("connection", (socket: Socket) => {
-    console.log("Se conectou", socket.id);
-});
-
-app.use(express.json());
-
-app.use(routes)
+import { http } from "./http";
+import "./websocket/client";
 
 http.listen(3000, () => console.log("Rodando... na porta 3000"));
